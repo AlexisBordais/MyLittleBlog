16da054d29d4ca3203879b45d454ec0b7b41e04f
alexis.bordais@gmail.com <alexis.bordais@gmail.com>
2017-01-19 14:26:04 +0100

    blogs called by json

--- public/js/components/blogsList/blogsList.js
+++ public/js/components/blogsList/blogsList.js
@@ -2,5 +2,16 @@
   'use strict';
   app.component('blogsList', {
     templateUrl: 'js/components/blogsList/blogsList.html',
+    controller: ['$http', function($http) {
+
+      this.blogs = []
+
+      $http.get('blogsList.json').then((response) => {
+          this.blogs = response.data
+      })
+
+
+
+    }]
   })
 })(angular.module('app.blogsList'))
